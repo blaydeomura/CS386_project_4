89c01587389a24663615173077f720f6a8141992
Blayde Omura <103175915+blaydeomura@users.noreply.github.com>
2024-04-24 00:43:55 -0700

    Reformatted

--- kernel_blayde.go
+++ kernel_blayde.go
@@ -9,17 +9,17 @@ import "fmt"
 // The state kept by the CPU in order to implement kernel support.
 type kernelCpuState struct {
 	// TODO: Fill this in.
-    kernelMode bool       // True if in kernel mode, false if in user mode.
-    timerCount uint32     // Count of instructions executed for timer management.
-    trapHandlerAddr uint32 // Static memory address where the trap handler is located.
+	kernelMode      bool   // True if in kernel mode, false if in user mode.
+	timerCount      uint32 // Count of instructions executed for timer management.
+	trapHandlerAddr uint32 // Static memory address where the trap handler is located.
 }
 
 // The initial kernel state when the CPU boots.
 var initKernelCpuState = kernelCpuState{
 	// TODO: Fill this in.
-    kernelMode: false,   // Start in user mode.
-    timerCount: 0,       // Timer count starts at 0.
-    trapHandlerAddr: 0x1000, // NEED TO PUT ADDRESS OF KERNEL MODE HERE?
+	kernelMode:      false,  // Start in user mode.
+	timerCount:      0,      // Timer count starts at 0.
+	trapHandlerAddr: 0x1000, // NEED TO PUT ADDRESS OF KERNEL MODE HERE?
 }
 
 // -----For preexecute hook, this is where all of the security concerns are done----
@@ -36,24 +36,24 @@ var initKernelCpuState = kernelCpuState{
 // will immediately return without any further execution.
 func (k *kernelCpuState) preExecuteHook(c *cpu) (bool, error) {
 	// TODO: Fill this in.
-    // Increment the instruction timer on each CPU step.
-    k.timerCount++
-    
+	// Increment the instruction timer on each CPU step.
+	k.timerCount++
+
 	// Security check 1:
-    // Check for timer interrupt every 128 instructions.
-    if k.timerCount % 128 == 0 {
-        fmt.Println("\nTimer fired!\n")
-        // TODO: Need to halt and interrupt here? need to switch to kernel mode?
-    }
+	// Check for timer interrupt every 128 instructions.
+	if k.timerCount%128 == 0 {
+		fmt.Println("\nTimer fired!\n")
+		// TODO: Need to halt and interrupt here? need to switch to kernel mode?
+	}
 
 	// Secruity check 2:
-    // Check for privileged instructions if in user mode.
-    if !k.kernelMode {
-        currentInstr := c.memory[c.registers[7]] // Assuming IP is in register 7.
-        if currentInstr.IsPrivileged() {
-            return false, fmt.Errorf("Illegal instruction access in user mode")
-        }
-    }
+	// Check for privileged instructions if in user mode.
+	if !k.kernelMode {
+		currentInstr := c.memory[c.registers[7]] // Assuming IP is in register 7.
+		if currentInstr.IsPrivileged() {
+			return false, fmt.Errorf("Illegal instruction access in user mode")
+		}
+	}
 
 	// ADD MORE Checks or state update below-------
 
@@ -91,47 +91,45 @@ func init() {
 	}
 
 	// TODO: Add hooks to other existing instructions to implement kernel support.
-	
+
 	//TODO: Hook for write instruction to check for privellages
 	instrWrite.addHook(func(c *cpu, args [3]byte) (bool, error) {
-        if !c.kernel.kernelMode {
-            return true, fmt.Errorf("write operation not allowed in user mode")
-        }
-        return false, nil
-    })
+		if !c.kernel.kernelMode {
+			return true, fmt.Errorf("write operation not allowed in user mode")
+		}
+		return false, nil
+	})
 
 	// TODO: Ensure that 'load' can only access memory within bounds
-    instrLoad.addHook(func(c *cpu, args [3]byte) (bool, error) {
-        addr := resolveArg(c, args[0])
-        if addr < 1024 || addr >= 2048 {
-            return true, fmt.Errorf("\nOut of bounds memory access!\n")
-        }
-        return false, nil
-    })
-	
+	instrLoad.addHook(func(c *cpu, args [3]byte) (bool, error) {
+		addr := resolveArg(c, args[0])
+		if addr < 1024 || addr >= 2048 {
+			return true, fmt.Errorf("\nOut of bounds memory access!\n")
+		}
+		return false, nil
+	})
 
 	// TODO: Ensure that 'store' can only access memory within bounds
 	instrStore.addHook(func(c *cpu, args [3]byte) (bool, error) {
-        addr := resolveArg(c, args[1])
-        if addr < 1024 || addr >= 2048 {
-            return true, fmt.Errorf("\nOut of bounds memory access!\n")
-        }
-        return false, nil
-    })
+		addr := resolveArg(c, args[1])
+		if addr < 1024 || addr >= 2048 {
+			return true, fmt.Errorf("\nOut of bounds memory access!\n")
+		}
+		return false, nil
+	})
 
 	// TODO: Privellaged instructions like 'halt', 'read', 'write' should only be executed in kernel
 	// For syscall essentially
 	instrSyscall.addHook(func(c *cpu, args [3]byte) (bool, error) {
-        if !c.kernel.kernelMode {
-            // Switch to kernel mode and set the trap handler address
-            c.kernel.kernelMode = true
-            c.kernel.trapHandlerAddr = c.registers[7]
-            c.registers[7] = k.trapHandlerAddr // Jump to the trap handler
-            return true, nil // Skip the current instruction execution as we handle the mode switch
-        }
-        return false, nil
-    })
-
+		if !c.kernel.kernelMode {
+			// Switch to kernel mode and set the trap handler address
+			c.kernel.kernelMode = true
+			c.kernel.trapHandlerAddr = c.registers[7]
+			c.registers[7] = k.trapHandlerAddr // Jump to the trap handler
+			return true, nil                   // Skip the current instruction execution as we handle the mode switch
+		}
+		return false, nil
+	})
 
 	//------ Instructions below ----
 
@@ -153,13 +151,12 @@ func init() {
 		instrSyscall = &instr{
 			name: "syscall",
 			cb: func(c *cpu, args [3]byte) error {
-				syscallNumber := int(args[0] & 0x7F)  // Mask out the high bit to get the correct syscall number
+				syscallNumber := int(args[0] & 0x7F) // Mask out the high bit to get the correct syscall number
 				fmt.Println("Executing syscall number: ", syscallNumber)
-				
+
 				// TODO: ADD IN a check to see if we're in kernel mode. If not, get into it
 
-
-				switch syscallNumber {  
+				switch syscallNumber {
 				case 0: // read syscall
 					var buf [1]byte
 					_, err := c.read.Read(buf[:])
@@ -168,26 +165,26 @@ func init() {
 					}
 					c.registers[6] = word(buf[0]) // Store the read byte into register 6
 					return nil
-		
+
 				case 1: // write syscall
-					b := byte(c.registers[6] & 0xFF)    // Get the least significant byte from register 6
+					b := byte(c.registers[6] & 0xFF) // Get the least significant byte from register 6
 					_, err := c.write.Write([]byte{b})
 					if err != nil {
 						return fmt.Errorf("write error: %v", err)
 					}
 					return nil
-		
+
 				case 2: // exit syscall
 					fmt.Println("Program has exited")
 					c.halted = true // Halt the CPU
 					return nil
-		
+
 				default:
 					return fmt.Errorf("unknown syscall number %d", syscallNumber) // Use the masked number in error messages too
 				}
 			},
 			validate: func(args [3]byte) error {
-				syscallNumber := int(args[0] & 0x7F)  // Mask out the high bit
+				syscallNumber := int(args[0] & 0x7F) // Mask out the high bit
 				if syscallNumber > 2 {
 					return fmt.Errorf("invalid syscall number %d", syscallNumber)
 				}
