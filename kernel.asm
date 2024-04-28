ea0b82911322e65dbacc7f106951894d997449b9
Tomby68 <tcsorkin@dons.usfca.edu>
2024-04-28 16:06:44 -0700

    Add kernel assembly file

---
+++ kernel.asm
@@ -0,0 +1,119 @@
+main:
+	setTrapHandler .trapHandler
+
+; Store first two bytes of input into r0
+bootloader:
+	read r0							; read 1/2 bytes
+	shl r0 8 r0						; shift left 8bits
+	read r1							; read 2/2 bytes
+	or r0 r1 r0
+
+	loadLiteral 1024 r2				; r2: 1024, just preserves the amount we need to buffer the program by
+
+	loadLiteral 0 r3				; make r3 to 0
+
+; Next: Keep reading from input into r1 until we get 0
+loop:
+	; First: Read a word (4 bytes) into r1, r2, r3, and r4
+	loadLiteral 0 r4				; make r4 to 0
+	
+	read r1							; read r1 because it'll overwrite
+	shl r1 24 r1					; shift left r1 to 24 and store that into r1
+	or r4 r1 r4						; r4 || r1 and store that into r4
+	
+	read r1							; read r1
+	shl r1 16 r1					; shift r1 left 16 
+	or r4 r1 r4						; combine r1 into r4
+
+	read r1							; repeat
+	shl r1 8 r1
+	or r4 r1 r4
+	
+	read r1							; no need to shift this time
+	or r4 r1 r4
+
+	store r4 r2						; store the word into memory address
+
+	load r2 r4						; load r2 to r4
+	
+	add r2 1 r2						; add 1 to r2
+	add r3 1 r3						; add 1 to r3
+
+	lt r3 r0 r4		; Check if r4, our word counter, is equal to r0, the program length
+	
+	cmove r4 .loop r7
+
+	changeMode 0
+	loadLiteral 1024 r7		; Move the iptr to 1024, where the program starts
+
+trapHandler:
+	; Store the CPU program state in memory before executing trap handler
+
+	store r0 0
+	store r1 4
+	store r2 8
+	store r3 12
+	store r4 16
+	store r5 20
+
+	load 24 r5
+	lt r5 3 r0		; If r6 < 3: r6 == 0, 1, or 2, so it is a syscall
+	cmove r0 .syscallHandler r7
+
+syscallHandler:
+	; If r6 == 0, execute a read instruction
+	; If r6 == 1, execute a write instruction
+	; If r6 == 2, execute a halt instruction
+	
+	eq r5 0 r0
+	cmove r0 .readInstr r7
+	eq r5 1 r0
+	cmove r0 .writeInstr r7
+	eq r5 2 r0
+	cmove r0 .haltInstr r7
+
+	; Add an exitTrapHandler here, just in case r6 is somehow < 0
+
+readInstr:
+	read r6
+	move .exitTrapHandler r7
+
+writeInstr:
+	write r6
+	move .exitTrapHandler r7
+	
+haltInstr:
+	write 'P'
+	write 'r'
+	write 'o'
+	write 'g'
+	write 'r'
+	write 'a'
+	write 'm'
+	write 32
+	write 'h'
+	write 'a'
+	write 's'
+	write 32
+	write 'e'
+	write 'x'
+	write 'i'
+	write 't'
+	write 'e'
+	write 'd'
+	halt
+
+exitTrapHandler:
+	; When exiting the trapHandler: Restore all register states
+	load 0 r0
+	load 4 r1
+	load 8 r2
+	load 12 r3
+	load 16 r4
+	load 20 r5
+
+	; Then: Restore the iptr back to where it was in the program
+	; In kernel.go, we stored the correct iptr value at memory
+	; location 28
+
+	changeMode 0 28
--- kernel.go
+++ kernel.go
@@ -9,13 +9,21 @@ import "fmt"
 // The state kept by the CPU in order to implement kernel support.
 type kernelCpuState struct {
 	// TODO: Fill this in.
+	kernelMode      bool   // True if in kernel mode, false if in user mode.
+	timerCount      uint32 // Count of instructions executed for timer management.
+	trapHandlerAddr word // Static memory address where the trap handler is located.
 }
 
 // The initial kernel state when the CPU boots.
 var initKernelCpuState = kernelCpuState{
 	// TODO: Fill this in.
+	kernelMode:      true,  // Start in kernel mode.
+	timerCount:      0,      // Timer count starts at 0.
+	trapHandlerAddr: 0, // TODO: NEED TO PUT ADDRESS OF KERNEL MODE HERE?
 }
 
+// -----For preexecute hook, this is where all of the security concerns are done----
+// -----ie making sure enough space allocation, others listed on the document-------
 // A hook which is executed at the beginning of each instruction step.
 //
 // This permits the kernel support subsystem to perform extra validation that is
@@ -28,6 +36,37 @@ var initKernelCpuState = kernelCpuState{
 // will immediately return without any further execution.
 func (k *kernelCpuState) preExecuteHook(c *cpu) (bool, error) {
 	// TODO: Fill this in.
+	// TODO: If syscall is called, make sure in kernel mode
+    // if is a syscall // Assuming isSyscall checks if current instruction is a syscall
+    //     iptr registers[7] = k.trapHandlerAddr // Set instruction pointer to trap handler address
+    //     return true, nil // Skip normal execution to handle the syscall in kernel mode
+    // }
+
+
+	// Increment the instruction timer on each CPU step.
+	//k.timerCount++
+
+	// Security check 2:
+	// Check for timer interrupt every 128 instructions.
+	//if k.timerCount % 128 == 0 {
+	//	fmt.Println("\nTimer fired!\n")
+		// TODO: Need to halt and interrupt here? need to switch to kernel mode?
+	//}
+
+	// Secruity check 3:
+	// Check for privileged instructions if in user mode.
+	//if !k.kernelMode {
+		// currentInstr := c.memory[c.registers[7]] // Assuming IP is in register 7.
+		//TODO: check if this instruction has privilleges
+		// if currentInstr.IsPrivileged() {
+		// 	return false, fmt.Errorf("Illegal instruction access in user mode")
+		// }
+	//}
+
+	// ADD MORE Checks or state update below-------
+
+	// TODO: Manage the behavior when this pre execute hook fails
+
 	return false, nil
 }
 
@@ -57,9 +96,59 @@ func init() {
 		})
 	}
 
-	// TODO: Add hooks to other existing instructions to implement kernel
-	// support.
+	// TODO: Add hooks to other existing instructions to implement kernel support.
 
+	//TODO: Hook for write instruction to check for privellages
+	instrWrite.addHook(func(c *cpu, args [3]byte) (bool, error) {
+		if !c.kernel.kernelMode {
+			return true, fmt.Errorf("write operation not allowed in user mode")
+		}
+		return false, nil
+	})
+
+	// TODO: If called in userland, ensure that 'load' can only access memory within bounds
+	instrLoad.addHook(func(c *cpu, args [3]byte) (bool, error) {
+		if !c.kernel.kernelMode {
+			addr := resolveArg(c, args[0])
+			if addr < 1024 || addr >= 2048 {
+				return true, fmt.Errorf("\nOut of bounds memory access!\n")
+			}
+		}
+		return false, nil
+	})
+
+	// TODO: If called in userland, ensure that 'store' can only access memory within bounds
+	instrStore.addHook(func(c *cpu, args [3]byte) (bool, error) {
+		if !c.kernel.kernelMode {
+			addr := resolveArg(c, args[1])
+			if addr < 1024 || addr >= 2048 {
+				return true, fmt.Errorf("\nOut of bounds memory access!\n")
+			}
+		}
+		return false, nil
+	})
+
+	//TODO: implement hook for read
+
+	//TODO: implement hook for halt
+
+	// TODO: Privellaged instructions like 'halt', 'read', 'write' should only be executed in kernel
+	// For syscall essentially
+	// instrSyscallCheck.addHook(func(c *cpu, args [3]byte) (bool, error) {
+	// 	if !c.kernel.kernelMode {
+	// 		// Switch to kernel mode and set the trap handler address
+	// 		c.kernel.kernelMode = true
+	// 		//TODO: Get register 7 iptr state
+	// 		//TODO: GET TRAP HANDLER ADDRESS
+	// 		return true, nil                   // Skip the current instruction execution as we handle the mode switch
+	// 	}
+	// 	return false, nil
+	// })
+
+	//------ Instructions below ----
+
+	// ---At 4/18, we test by running: go run *.go bootloader.asm prime.asm---
+	// Syscall is how a process asks kernel to do something
 	var (
 		// syscall <code>
 		//
@@ -76,15 +165,81 @@ func init() {
 		instrSyscall = &instr{
 			name: "syscall",
 			cb: func(c *cpu, args [3]byte) error {
-				// TODO: Fill this in.
-				return fmt.Errorf("unimplemented")
+				syscallNumber := int(args[0] & 0x7F) // Mask out the high bit to get the correct syscall number
+				//fmt.Println("Executing syscall number: ", syscallNumber)
+
+				// TODO: ADD IN a check to see if we're in kernel mode. If not, get into it
+				if syscallNumber > 2 || syscallNumber < 0 {
+					return fmt.Errorf("invalid syscall number %d", syscallNumber)
+				}
+
+				c.memory[24] = word(syscallNumber)
+				c.memory[28] = c.registers[7]
+				c.registers[7] = c.kernel.trapHandlerAddr
+				c.kernel.kernelMode = true
+				//fmt.Println("Moved into trap handler...")
+				return nil
 			},
-			validate: nil,
+			validate: genValidate(regOrLit, ignore, ignore),
+
+			/*func(args [3]byte) error {
+				if syscallNumber > 2 {
+					return fmt.Errorf("invalid syscall number %d", syscallNumber)
+				}
+				return nil 
+			} */
 		}
 
 		// TODO: Add other instructions that can be used to implement a kernel.
+
+		// setTrapHandler takes one argument, which should be the trapHandler address in kernel.asm.
+		// setTrapHandler sets the kernel trapHandlerAddr to args[0]
+		instrSetTrapHandler = &instr{
+			name: "setTrapHandler",
+			cb: func(c *cpu, args [3]byte) error {
+				fmt.Print("Hello")
+				c.kernel.trapHandlerAddr = resolveArg(c, args[0])
+				return nil
+			},
+			validate: genValidate(regOrLit, ignore, ignore),
+		}
+
+
+		// instrChangeMode can take 1 or 2 args. 
+		// It is meant to change kernel modes and to switch back into the user program at the same time
+		// instrChangeMode checks first if the kernel is in kernelMode or not. If not, it does nothing
+		// If it is called in kernel mode, instrChangeMode changes the kernelMode to parameter 1, which should be either
+		// 0 or 1.
+		// 0: Switch to user mode, so kernelMode = false
+		// 1: Switch to kernel mode, so kernelMode = true
+		// Then, if the second argument passed to instrChangeMode is not empty, it loads the value at args[1] and 
+		// sets the iptr to that value
+		instrChangeMode = &instr{
+			name: "changeMode",
+			cb: func(c *cpu, args [3]byte) error {
+				if c.kernel.kernelMode {
+					newMode := int(resolveArg(c, args[0]))
+					if newMode == 0 {
+						c.kernel.kernelMode = false
+					} else if newMode == 1 {
+						c.kernel.kernelMode = true
+					} else {
+						return fmt.Errorf("Invalid kernel mode %d", newMode)
+					}
+
+					branchAddr := int(args[1] & 0x7F)
+					if branchAddr != 0 {
+						c.registers[7] = c.memory[branchAddr]
+					}
+				}
+				return nil
+			},
+			validate: genValidate(regOrLit, regOrLit, ignore),
+		}
 	)
 
 	// Add kernel instructions to the instruction set.
 	instructionSet.add(instrSyscall)
+	instructionSet.add(instrSetTrapHandler)
+	instructionSet.add(instrChangeMode)
 }
